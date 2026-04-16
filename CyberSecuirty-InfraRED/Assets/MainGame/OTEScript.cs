6aee67f5f599336ea11a3e606711628a18274664
DylanoSpks <dylanored@yahoo.com>
2026-04-16 02:12:49 +0200

    Changes

--- CyberSecuirty-InfraRED/Assets/MainGame/OTEScript.cs
+++ CyberSecuirty-InfraRED/Assets/MainGame/OTEScript.cs
@@ -4,10 +4,14 @@ using UnityEngine.SceneManagement;
 public class OTEScript : MonoBehaviour
 {
     [SerializeField] private string sceneToLoad;
+    [SerializeField] private GameObject lightOnn;
+    [SerializeField] private GameObject lightOff;
 
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered");
         SceneManager.LoadScene(sceneToLoad);
+        lightOnn.SetActive(false);
+        lightOff.SetActive(true);
     }
 }
