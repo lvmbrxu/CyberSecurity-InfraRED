ea2dbaa9a74b27347f8f28d5fe53e558dd4d7df9
DylanoSpks <dylanored@yahoo.com>
2026-06-02 14:56:48 +0200

    OTE

--- CyberSecuirty-InfraRED/Assets/MainGame/Scripts_MainGame/OTEScript.cs
+++ CyberSecuirty-InfraRED/Assets/MainGame/Scripts_MainGame/OTEScript.cs
@@ -6,6 +6,19 @@ public class OTEScript : MonoBehaviour
     [SerializeField] private string sceneToLoad;
     [SerializeField] private GameObject lightOnn;
     [SerializeField] private GameObject lightOff;
+    [SerializeField] private GameObject lightOnn2;
+    [SerializeField] private GameObject lightOff2;
+    [SerializeField] private GameObject lightObjectOnn;
+    [SerializeField] private GameObject lightObjectOff;
+
+    [SerializeField] private GameObject nextGameGL;
+    [SerializeField] private GameObject nextGameGL2;
+    [SerializeField] private GameObject nextGameRL;
+    [SerializeField] private GameObject nextGameRL2;
+    [SerializeField] private GameObject nextGameObjectOnn;
+    [SerializeField] private GameObject nextGameObjectOff;
+
+    [SerializeField] private GameObject invisWall;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -13,5 +26,18 @@ public class OTEScript : MonoBehaviour
         SceneManager.LoadScene(sceneToLoad);
         lightOnn.SetActive(false);
         lightOff.SetActive(true);
+        lightOnn2.SetActive(false);
+        lightOff2.SetActive(true);
+        lightObjectOnn.SetActive(false);
+        lightObjectOff.SetActive(true);
+        
+        nextGameGL.SetActive(true);
+        nextGameGL2.SetActive(true);
+        nextGameRL.SetActive(false);
+        nextGameRL2.SetActive(false);
+        nextGameObjectOnn.SetActive(true);
+        nextGameObjectOff.SetActive(false);
+        
+        invisWall.SetActive(false);
     }
 }
